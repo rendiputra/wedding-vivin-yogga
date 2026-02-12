cfc0c256fe7feeffd6028b556bbcbbc980c808ad
rendiputra <rendiputrapradana@gmail.com>
2026-02-12 21:58:41 +0700

    create design

---
+++ src/data/content.ts
@@ -0,0 +1,48 @@
+export const weddingData = {
+  couple: {
+    bride: {
+      nickname: "Vivin",
+      fullName: "Vivin Hanjayani S. Pd",
+      parents: "Putri dari Bpk. Merry Ahmadi & Ibu Hadijah",
+      instagram: "@vivinhnzy",
+      instagramUrl: "https://instagram.com/vivinhnzy",
+      photo: "/images/bride.png",
+    },
+    groom: {
+      nickname: "Yogga",
+      fullName: "Yogga Alfiansyah S R. S. AP",
+      parents: "Putra dari Bpk. Suryani E & Ibu. Endah Sutijiati",
+      instagram: "@yoggaalfian_",
+      instagramUrl: "https://instagram.com/yoggaalfian_",
+      photo: "/images/groom.png",
+    },
+  },
+  event: {
+    date: "Minggu, 26 April 2026",
+    isoDate: "2026-04-26T08:00:00+07:00",
+    mainLocation: "Kediaman Mempelai Wanita",
+    address:
+      "Graha Prima Blok IF RT 002 RW 011 Desa Satria Jaya, Kec. Tambun Utara, Kab. Bekasi",
+    akad: "08.00 WIB",
+    resepsi: "10.00 - 17.00 WIB",
+    mapsUrl:
+      "https://www.google.com/maps/search/Graha+Prima+Desa+Satria+Jaya+Tambun+Utara+Bekasi",
+    mapsEmbed:
+      "https://www.openstreetmap.org/export/embed.html?bbox=107.0%2C-6.3%2C107.1%2C-6.2&layer=mapnik",
+  },
+  meta: {
+    title: "The Wedding of Vivin & Yogga",
+    audio: "/audio/beautiful-in-white.mp3",
+    quote:
+      "Dan diantara bukti-bukti kebesaran Allah adalah diciptakan-Nya untukmu pasangan hidup dari jenismu sendiri, supaya kamu tentram di sampingnya, dan dijadikan-Nya kasih sayang di antara kamu. Sesungguhnya yang demikian itu menjadi tanda-tanda orang yang berfikir.",
+    quoteSource: "QS. Ar-Ruum: 21",
+  },
+  gallery: [
+    { src: "/images/gallery-1.png", alt: "Pre-wedding photo 1" },
+    { src: "/images/gallery-2.png", alt: "Pre-wedding photo 2" },
+    { src: "/images/gallery-3.png", alt: "Pre-wedding photo 3" },
+    { src: "/images/gallery-4.png", alt: "Pre-wedding photo 4" },
+    { src: "/images/gallery-5.png", alt: "Pre-wedding photo 5" },
+    { src: "/images/gallery-6.png", alt: "Pre-wedding photo 6" },
+  ],
+} as const;
